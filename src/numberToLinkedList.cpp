ecd9db79ddf913a6c65f55818bde7b133004d802
eduvulapatidivya <eduvulapatidivya@missionrnd.com>
2016-03-16 01:04:21 +0530

    Submit Commit

--- src/numberToLinkedList.cpp
+++ src/numberToLinkedList.cpp
@@ -19,6 +19,49 @@ struct node {
 	struct node *next;
 };
 
+struct node* insert1(int data, struct node *start1)
+{
+	struct node* nn1;
+	nn1 = (struct node*)malloc(sizeof(struct node));
+	nn1->num = data;
+	nn1->next = NULL;
+
+	if (start1 == NULL)
+	{
+
+		start1 = nn1;
+	}
+	else
+	{
+		nn1->next = start1;
+		start1 = nn1;
+
+	}
+
+	return start1;
+}
+
+
+
+
 struct node * numberToLinkedList(int N) {
-	return NULL;
+
+	int quo = 0;
+	struct node *start1 = NULL;
+	if (N < 0)
+	{
+		N = N*-1;
+	}
+	if (N == 0)
+	{
+		start1 = insert1(quo, start1);
+	}
+	while (N > 0)
+	{
+		quo = N % 10;
+		start1 = insert1(quo, start1);
+		N = N / 10;
+	}
+	return start1;
+
 }
